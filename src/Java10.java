010b38ba04c73a4a78911677ca6804119dcf9d7a
DESKTOP-JR81K3K\USER <huhu@naver.com>
2023-06-19 16:18:15 +0900

    7

---
+++ src/Java10.java
@@ -0,0 +1,12 @@
+
+public class Java10 {
+
+	public static void hiCar() {
+		System.out.println("안녕하세요 함수 입니다.");
+	}
+
+	public static void main(String[] args) {
+		hiCar();
+	}
+
+}
---
+++ src/Java9.java
@@ -0,0 +1,17 @@
+//javac java9.java
+//java9.class
+//java java9(.class)
+public class Java9 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("hello world");
+		}
+		hiEveryone();
+	}
+	
+	public static void hiEveryone() {
+		System.out.println("안녕하세요.");
+	}
+
+}
